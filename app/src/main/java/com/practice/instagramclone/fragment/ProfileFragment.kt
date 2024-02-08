9732a9f2c7c9efaf43dfb5617c47bfe8e12a606c
YashNagare <yashpnagare@gmail.com>
2024-02-09 00:05:55 +0530

    added fragments for homepage

---
+++ app/src/main/java/com/practice/instagramclone/fragment/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.practice.instagramclone.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.practice.instagramclone.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+    }
+}
