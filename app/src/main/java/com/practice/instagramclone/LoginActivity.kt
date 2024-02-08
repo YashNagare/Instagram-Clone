fc496341432e92f29373a012cd2868243cfd2f32
YashNagare <yashpnagare@gmail.com>
2024-02-09 00:07:55 +0530

    completed login activity

--- app/src/main/java/com/practice/instagramclone/LoginActivity.kt
+++ app/src/main/java/com/practice/instagramclone/LoginActivity.kt
@@ -1,11 +1,47 @@
 package com.practice.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.practice.instagramclone.databinding.ActivityLoginBinding
+import com.practice.instagramclone.models.User
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            if (binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")
+            ) {
+                Toast.makeText(this, "please fill all the details", Toast.LENGTH_SHORT).show()
+            } else {
+                var user = User(
+                    binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                )
+
+                Firebase.auth.signInWithEmailAndPassword(user.email!!, user.password!!)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            startActivity(Intent(this, HomeActivity::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(this, it.exception?.localizedMessage, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+            }
+        }
+
     }
 }
