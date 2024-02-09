005319570842798d9a9df051c62e5b24a1a497b8
YashNagare <yashpnagare@gmail.com>
2024-02-09 19:20:11 +0530

    added picasso library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
 
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
+    implementation("com.squareup.picasso:picasso:2.8")
 }
