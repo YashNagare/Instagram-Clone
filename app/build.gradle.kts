e186cf063b6f42f33a78cc1442855a5ff13da5c5
YashNagare <yashpnagare@gmail.com>
2024-02-12 23:11:35 +0530

    added roundedimageview dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -63,4 +63,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
 
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.makeramen:roundedimageview:2.3.0")
 }
