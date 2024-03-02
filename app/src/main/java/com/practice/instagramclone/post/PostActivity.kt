fcd8beb7155df8103b1b1e1c43430142aad7d2ef
YashNagare <yashpnagare@gmail.com>
2024-03-02 14:38:19 +0530

    added photo post feature

--- app/src/main/java/com/practice/instagramclone/post/PostActivity.kt
+++ app/src/main/java/com/practice/instagramclone/post/PostActivity.kt
@@ -1,13 +1,36 @@
 package com.practice.instagramclone.post
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+import com.practice.instagramclone.HomeActivity
 import com.practice.instagramclone.databinding.ActivityPostBinding
+import com.practice.instagramclone.models.Post
+import com.practice.instagramclone.utils.POST
+import com.practice.instagramclone.utils.POST_FOLDER
+import com.practice.instagramclone.utils.uploadImage
 
 class PostActivity : AppCompatActivity() {
 
     val binding by lazy {
         ActivityPostBinding.inflate(layoutInflater)
+    }
+
+    var imageUrl: String? = null
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        uri?.let { url ->
+            uploadImage(uri, POST_FOLDER) {
+                if (url != null) {
+                    binding.selectImage.setImageURI(uri)
+                    imageUrl = url.toString()
+                }
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +41,35 @@ class PostActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         binding.materialToolbar.setNavigationOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
             finish()
+        }
+
+        binding.selectImage.setOnClickListener {
+            launcher.launch("image/*")
+        }
+
+        binding.cancelButton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+
+        binding.postButton.setOnClickListener {
+            val post: Post = Post(
+                postUrl = imageUrl!!,
+                caption = binding.caption.text.toString(),
+                uid = Firebase.auth.currentUser!!.uid,
+                time = System.currentTimeMillis().toString()
+            )
+
+            Firebase.firestore.collection(POST).document().set(post).addOnSuccessListener {
+                Firebase.firestore.collection(Firebase.auth.currentUser!!.uid).document()
+                    .set(post)
+                    .addOnSuccessListener {
+                        startActivity(Intent(this@PostActivity, HomeActivity::class.java))
+                        finish()
+                    }
+            }
         }
     }
 }
