a0281674ab0b4cbb386d01845f621b8b44204425
YashNagare <yashpnagare@gmail.com>
2024-03-02 14:40:38 +0530

    added constant

--- app/src/main/java/com/practice/instagramclone/utils/Constant.kt
+++ app/src/main/java/com/practice/instagramclone/utils/Constant.kt
@@ -2,3 +2,8 @@ package com.practice.instagramclone.utils
 
 const val USER_NODE = "User"
 const val USER_PROFILE_FOLDER = "Profile"
+const val POST_FOLDER = "PostImages"
+const val REEL_FOLDER = "ReelVideos"
+const val POST = "Post"
+const val REEL = "Reel"
+const val FOLLOW="FOLLOW"
