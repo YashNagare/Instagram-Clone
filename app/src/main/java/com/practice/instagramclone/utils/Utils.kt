3518f08d72e9932cab381a5e006f9caf9d154441
YashNagare <yashpnagare@gmail.com>
2024-03-02 14:40:20 +0530

    added progress bar

--- app/src/main/java/com/practice/instagramclone/utils/Utils.kt
+++ app/src/main/java/com/practice/instagramclone/utils/Utils.kt
@@ -1,11 +1,13 @@
 package com.practice.instagramclone.utils
 
+import android.app.ProgressDialog
+import android.content.Context
 import android.net.Uri
 import com.google.firebase.storage.FirebaseStorage
 import java.util.UUID
 
-fun uploadImage(uri: Uri, folderName: String, callback: (String?) -> Unit){
-    var imageURL: String ?= null
+fun uploadImage(uri: Uri, folderName: String, callback: (String?) -> Unit) {
+    var imageURL: String? = null
     FirebaseStorage.getInstance().getReference(folderName).child(UUID.randomUUID().toString())
         .putFile(uri)
         .addOnSuccessListener {
@@ -13,5 +15,24 @@ fun uploadImage(uri: Uri, folderName: String, callback: (String?) -> Unit){
                 imageURL = it.toString()
                 callback(imageURL)
             }
+        }
+}
+
+fun uploadVideo(uri: Uri, folderName: String, progressDialog: ProgressDialog, callback: (String?) -> Unit) {
+    var imageURL: String? = null
+    progressDialog.setTitle("Uploading...")
+    progressDialog.show()
+    FirebaseStorage.getInstance().getReference(folderName).child(UUID.randomUUID().toString())
+        .putFile(uri)
+        .addOnSuccessListener {
+            it.storage.downloadUrl.addOnSuccessListener {
+                imageURL = it.toString()
+                progressDialog.dismiss()
+                callback(imageURL)
+            }
+        }
+        .addOnProgressListener {
+            val uploadedValue: Long = it.bytesTransferred/it.totalByteCount
+            progressDialog.setMessage("Uploaded $uploadedValue %")
         }
 }
